f8fe08319fb65a181ce7eed2809c354e027357ab
yahadimon8-deep <yahadimon8@gmail.com>
2026-07-05 02:52:34 +0300

    feat: implement Genre and Actor models with main logic, fix flake8 issues

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import init_django_orm  # noqa: F401
+from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
-def main():
-    # 1. Створення даних (Create)
+def main() -> QuerySet[Actor]:
+    # Create
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
-    Genre.objects.create(name="Drama")
+    Genre.objects.create(name="Dramma")
 
-    Actor.objects.create(first_name="George", last_name="Clooney")
-    Actor.objects.create(first_name="Keanu", last_name="Reeves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keagan")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
     Actor.objects.create(first_name="Will", last_name="Smith")
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
-    Genre.objects.filter(name="Drama").update(name="Drama")
+    # Update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(
-        first_name="George", last_name="Clooney"
+        first_name="George", last_name="Klooney"
     ).update(last_name="Clooney")
     Actor.objects.filter(
-        first_name="Keanu", last_name="Reeves"
+        first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
+
+    # Delete
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
+
+    # Return QuerySet of actors with
+    # last name "Smith" ordered by first name
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
