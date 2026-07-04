7017faf99e78c8a385b4536ac233448c5d7fce4c
yahadimon8-deep <yahadimon8@gmail.com>
2026-07-05 01:51:31 +0300

    Fix flake8 formatting and type annotations

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import init_django_orm  # noqa: F401
-from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
+def main():
     # 1. Створення даних (Create)
     Genre.objects.create(name="Western")
     Genre.objects.create(name="Action")
@@ -16,7 +15,6 @@ def main() -> QuerySet:
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
-    # 2. Оновлення даних (Update)
     Genre.objects.filter(name="Drama").update(name="Drama")
     Actor.objects.filter(
         first_name="George", last_name="Clooney"
@@ -24,12 +22,8 @@ def main() -> QuerySet:
     Actor.objects.filter(
         first_name="Keanu", last_name="Reeves"
     ).update(first_name="Keanu", last_name="Reeves")
-
-    # 3. Видалення даних (Delete)
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
-
-    # 4. Повернення відфільтрованого та відсортованого QuerySet
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
 
